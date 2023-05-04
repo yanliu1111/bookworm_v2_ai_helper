4b2ec4e18d91811428c239f4e493cbd34fe177f9
yanliu1111 <liuyan0504@gmail.com>
2023-05-03 19:49:57 -0600

    feat: install shadcn-ui  accordion twailwind merge

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Chat from "@/components/Chat";
 
 const inter = Inter({ subsets: ["latin"] });
 
---
+++ src/components/Chat.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+//FC = Functional Component
+const Chat: FC = () => {
+  return (
+    <div>
+      <h1>Chat</h1>
+    </div>
+  );
+};
+
+export default Chat;
