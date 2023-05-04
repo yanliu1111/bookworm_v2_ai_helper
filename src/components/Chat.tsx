58de67902c034757a5d9de5ce5254f1e08527b73
yanliu1111 <liuyan0504@gmail.com>
2023-05-03 23:24:31 -0600

    feat: AI helper build

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,10 +1,28 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import { FC } from "react";
+import ChatHeader from "./ChatHeader";
 //FC = Functional Component
 const Chat: FC = () => {
   return (
-    <div>
-      <h1>Chat</h1>
-    </div>
+    <Accordion
+      type="single"
+      collapsible
+      className="ralative bg-white z-40 shadow"
+    >
+      <AccordionItem value="item-1">
+        <div className="fixed right-8 w-80 bottom-8 bg-white border border-gray-200 rounded-md overflow-hidden">
+          <div className="w-full h-full flex flex-col">
+            <AccordionTrigger className="px-6 border-b border-zinc-300">
+              <ChatHeader />
+            </AccordionTrigger>
+          </div>
+        </div>
+      </AccordionItem>
+    </Accordion>
   );
 };
 
