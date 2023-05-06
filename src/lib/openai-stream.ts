51696b034b1781f29bbb017d017a61a75754ec49
yanliu1111 <liuyan0504@gmail.com>
2023-05-06 01:12:30 -0600

    Bugfix: typo

--- src/lib/openai-stream.ts
+++ src/lib/openai-stream.ts
@@ -27,7 +27,7 @@ export async function OpenAIStream(payload: OpenAIStreamPayload) {
 
   let counter = 0;
 
-  const res = await fetch("https://api.openai.com/v1/chat/completion", {
+  const res = await fetch("https://api.openai.com/v1/chat/completions", {
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
