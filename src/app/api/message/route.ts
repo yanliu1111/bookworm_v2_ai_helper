8706fbe933dc7fe9b6b37e7b8d508efff3005518
yanliu1111 <liuyan0504@gmail.com>
2023-05-05 21:13:33 -0600

    feat: openai stream done

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,5 +1,5 @@
 import { chatbotPrompt } from "@/app/helpers/constants/chatbot-prompt";
-import { ChateGTPMessage } from "@/lib/openai-stream";
+import { ChateGTPMessage, OpenAIStreamPayload } from "@/lib/openai-stream";
 import { MessageArraySchema } from "@/lib/validators/message";
 
 export async function POST(req: Request) {
@@ -11,4 +11,20 @@ export async function POST(req: Request) {
     content: message.text,
   }));
   outboundMessages.unshift({ role: "system", content: chatbotPrompt });
+
+  const payload: OpenAIStreamPayload = {
+    model: "gpt-3.5-turbo",
+    messages: outboundMessages,
+    temperature: 0.4,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 150,
+    stream: true,
+    n: 1,
+  };
+  //return the stream to the frontend
+  const stream = await OpenAIStream(payload);
+
+  return new Response(stream);
 }
--- src/lib/openai-stream.ts
+++ src/lib/openai-stream.ts
@@ -4,3 +4,14 @@ export interface ChateGTPMessage {
   role: ChatGPTAgent;
   content: string;
 }
+export interface OpenAIStreamPayload {
+  model: string;
+  messages: ChateGTPMessage[];
+  temperature: number;
+  top_p: number;
+  frequency_penalty: number;
+  presence_penalty: number;
+  max_tokens: number;
+  stream: boolean;
+  n: number;
+}
